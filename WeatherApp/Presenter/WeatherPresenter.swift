1c3a5aec4256421c343a72308a35a65455165509
vdrabll <fedosova.univ@gmail.com>
2023-05-03 17:02:39 +0300

    some little fixes

--- WeatherApp/Presenter/WeatherPresenter.swift
+++ WeatherApp/Presenter/WeatherPresenter.swift
@@ -6,27 +6,26 @@ import Foundation
 
 class WeatherPresenter: WeatherPresenterProtocol {
 
-    var networkService: NerworkServicesProtocol?
+    var networkService = WeatherNetworkServices()
     var view: WeatherMainViewProtocol?
 
     init(view: WeatherMainViewProtocol ) {
         self.view = view
-        self.networkService = WeatherNetworkServices()
     }
     
     
     func sendForcastData(complitionHandler: @escaping (ForcastDataDTO) -> ()) {
-        networkService?.getWeatherData { dto in
-            var forcast = ForcastDataDTO(icon: String(dto.fact.icon),
-                    name:String(dto.info.tzinfo.name) ,
-                    temp: String(dto.fact.temp),
-                    humidity: String(dto.fact.humidity),
-                    uv: String(dto.fact.uvIndex),
-                    rain: String(dto.fact.precProb),
-                    pressure: String(dto.fact.pressureMm),
-                    sunset: String(dto.forecasts[0].sunset),
-                    sunrise: String(dto.forecasts[0].sunrise))
-            complitionHandler(forcast)
+        networkService.getWeatherData { WeatherDataDTO in
+            let data = ForcastDataDTO(icon: String(WeatherDataDTO.fact.icon),
+                    name:String(WeatherDataDTO.info.tzinfo.name) ,
+                    temp: String(WeatherDataDTO.fact.temp),
+                    humidity: String(WeatherDataDTO.fact.humidity),
+                    uv: String(WeatherDataDTO.fact.uvIndex),
+                    rain: String(WeatherDataDTO.fact.precProb),
+                    pressure: String(WeatherDataDTO.fact.pressureMm),
+                    sunset: String(WeatherDataDTO.forecasts[0].sunset),
+                    sunrise: String(WeatherDataDTO.forecasts[0].sunrise))
+            complitionHandler(data)
         }
     }
 }
